c8dedc385482595e7f9fe64807797018d4146709
Sharif Abdullahi <55369861+saa-git@users.noreply.github.com>
2025-07-25 22:26:56 -0400

    Update hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -9,7 +9,8 @@
       availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
       kernelModules = [ ];
       luks.devices = {
-        "luks-1a4da71d-61f5-4762-8356-e175090d5786".device = "/dev/disk/by-uuid/1a4da71d-61f5-4762-8356-e175090d5786";
+        "nix".device = "";
+        "swap".device = "";
       };
     };
     kernelModules = [ "kvm-intel" ];
@@ -17,11 +18,11 @@
   
   fileSystems = {
     "/" = {
-      device = "/dev/mapper/luks-1a4da71d-61f5-4762-8356-e175090d5786";
+      device = "/dev/disk/by-label/NixOS";
       fsType = "btrfs";
       options = [ "subvol=@" ];
     };
-    "/boot" = { device = "/dev/disk/by-uuid/AA47-6C3A";
+    "/boot" = { device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
@@ -31,5 +32,5 @@
   
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-  swapDevices = [ { device = "/dev/mapper/luks-c76672c5-ac01-4d02-9fa3-eb2538607d77"; } ];
+  swapDevices = [ { device = "/dev/disk/by-label/SWAP"; } ];
 }
